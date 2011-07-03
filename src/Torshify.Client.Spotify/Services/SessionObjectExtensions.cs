d6b851b2829335b2c96f4a21981542f52fdb5139
Torstein Auensen <torshy@gmail.com>
2011-07-03 12:46:53 +0200

    Added extension method

--- src/Torshify.Client.Spotify/Services/SessionObjectExtensions.cs
+++ src/Torshify.Client.Spotify/Services/SessionObjectExtensions.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace Torshify.Client.Spotify.Services
+{
+    public static class SessionObjectExtensions
+    {
+        public static bool IsValid(this ISessionObject sessionObject)
+        {
+            // TODO : Workaround of a lacking feature in torshify-lib. Not possible to see if the track is valid or not. 
+            // The gethashcode will return 0 for invalid objects now
+            return sessionObject.GetHashCode() > 0;
+        }
+    }
+}
